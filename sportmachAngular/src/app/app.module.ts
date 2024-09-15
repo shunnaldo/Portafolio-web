6817e2d961e8efe172fec57a948fe0ffa2784209
shunnaldo <benjaminignacioparramolina@gmail.com>
2024-09-15 20:24:47 -0300

    Proyecto subido
    Se sube angular y firebase

---
+++ sportmachAngular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ sportmachAngular/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production: false,
+    firebaseConfig:{
+    apiKey: "AIzaSyBYGCbdr-FSDMtB_-PvDKf7OIJuo1-a5nE",
+    authDomain: "sportmach-fc07f.firebaseapp.com",
+    projectId: "sportmach-fc07f",
+    storageBucket: "sportmach-fc07f.appspot.com",
+    messagingSenderId: "276809292205",
+    appId: "1:276809292205:web:fcef7335a06702d4496924"
+    }
+};
