f255bee53de05df289e49bc82a77f714c14e0356
shunnaldo <benjaminignacioparramolina@gmail.com>
2024-10-01 21:22:09 -0300

    si

--- sportmachAngular/src/app/components/sectores/sectores.component.ts
+++ sportmachAngular/src/app/components/sectores/sectores.component.ts
@@ -18,7 +18,7 @@ export class SectoresComponent implements OnInit {
   dias: string[] = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
   horariosPorDia: { [key: string]: number[] } = {};
   horariosSeleccionados: { [key: string]: { inicio: number; fin: number }[] } = {};
-  duracionesPorDia: { [key: string]: number } = {}; // Nueva propiedad para las duraciones por día
+  duracionesPorDia: { [key: string]: number } = {};
   diaSeleccionado: string = '';
   modalAbierto: boolean = false;
 
@@ -27,63 +27,9 @@ export class SectoresComponent implements OnInit {
   ngOnInit(): void {
     this.sectores$ = this.sectoresService.getSectores();
     this.dias.forEach(dia => {
-      this.duracionesPorDia[dia] = 1; // Duración predeterminada para cada día
+      this.duracionesPorDia[dia] = 1;
     });
     this.resetHorarios();
-  }
-
-  resetHorarios(dia?: string) {
-    if (dia) {
-      this.generarHorariosPorDia(dia);
-    } else {
-      this.dias.forEach(dia => this.generarHorariosPorDia(dia));
-    }
-  }
-
-  generarHorariosPorDia(dia: string) {
-    const horasPorDia = [];
-    let horaActual = 9; // Comienza a las 9:00
-    const horaFinDia = 21; // Termina a las 21:00
-    const duracion = this.duracionesPorDia[dia];
-
-    while (horaActual + duracion <= horaFinDia) {
-      horasPorDia.push(horaActual);
-      horaActual += duracion;
-    }
-
-    this.horariosPorDia[dia] = horasPorDia;
-    this.horariosSeleccionados[dia] = this.horariosSeleccionados[dia] || [];
-  }
-
-  // Verificar si un horario ya está seleccionado
-  isHorarioSelected(dia: string, hora: number): boolean {
-    return this.horariosSeleccionados[dia]?.some(h => h.inicio === hora) ?? false;
-  }
-
-  // Seleccionar o deseleccionar un horario con duración personalizada
-  toggleHorario(dia: string, hora: number) {
-    const index = this.horariosSeleccionados[dia].findIndex(h => h.inicio === hora);
-
-    if (index === -1) {
-      const horaFin = hora + this.duracionesPorDia[dia];
-      this.horariosSeleccionados[dia].push({ inicio: hora, fin: horaFin });
-    } else {
-      this.horariosSeleccionados[dia].splice(index, 1);
-    }
-  }
-
-  guardarHorariosPorDia() {
-    const horariosDiaSeleccionado = this.horariosSeleccionados[this.diaSeleccionado].map(h => ({
-      dia: this.diaSeleccionado,
-      inicio: h.inicio,
-      fin: h.fin
-    }));
-
-    this.newSector.horarios = this.newSector.horarios.filter(h => h.dia !== this.diaSeleccionado);
-    this.newSector.horarios.push(...horariosDiaSeleccionado);
-
-    this.snackBar.open(`Horarios para ${this.diaSeleccionado} guardados`, 'Cerrar', { duration: 2000 });
-    this.cerrarModal();
   }
 
   // Método para agregar o actualizar un sector
@@ -109,6 +55,27 @@ export class SectoresComponent implements OnInit {
     }
   }
 
+  // Método para manejar la carga de archivos
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+    }
+  }
+
+  // Método para resetear el formulario
+  resetForm() {
+    this.newSector = new Sectores('', '', null, []);
+    this.selectedFile = null;
+    this.isEditing = false;
+    this.editingSectorId = null;
+    this.diaSeleccionado = '';
+    this.dias.forEach(dia => {
+      this.duracionesPorDia[dia] = 1;
+    });
+    this.resetHorarios();
+  }
+
   // Método para eliminar un sector
   deleteSector(idSector: string) {
     if (confirm('¿Estás seguro de que deseas eliminar este sector?')) {
@@ -128,7 +95,7 @@ export class SectoresComponent implements OnInit {
     this.editingSectorId = sector.idSector;
     this.newSector = { ...sector };
     this.dias.forEach(dia => {
-      this.duracionesPorDia[dia] = 1; // Restablecer duraciones
+      this.duracionesPorDia[dia] = 1;
     });
     sector.horarios.forEach(horario => {
       this.horariosSeleccionados[horario.dia] = [
@@ -138,37 +105,85 @@ export class SectoresComponent implements OnInit {
     });
   }
 
-  resetForm() {
-    this.newSector = new Sectores('', '', null, []);
-    this.selectedFile = null;
-    this.isEditing = false;
-    this.editingSectorId = null;
-    this.diaSeleccionado = '';
-    this.dias.forEach(dia => {
-      this.duracionesPorDia[dia] = 1;
-    });
-    this.resetHorarios();
+  // Método para verificar si un horario ya está seleccionado
+  isHorarioSelected(dia: string, hora: number): boolean {
+    return this.horariosSeleccionados[dia]?.some(h => h.inicio === hora) ?? false;
   }
 
-  onFileChange(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      this.selectedFile = file;
-    }
+  // Método para formatear las horas de manera legible
+  formatHour(hour: number): string {
+    const hours = Math.floor(hour);
+    const minutes = (hour % 1) * 60;
+    return minutes > 0 ? `${hours}:${minutes === 30 ? '30' : '00'}` : `${hours}:00`;
+  }
+
+  // Otros métodos para el manejo de horarios y la lógica de abrir/cerrar modal
+  abrirModal(dia: string) {
+    this.diaSeleccionado = dia;
+    this.modalAbierto = true;
   }
 
   cerrarModal() {
     this.modalAbierto = false;
   }
 
-  abrirModal(dia: string) {
-    this.diaSeleccionado = dia;
-    this.modalAbierto = true;
+  resetHorarios(dia?: string) {
+    if (dia) {
+      this.generarHorariosPorDia(dia);
+    } else {
+      this.dias.forEach(dia => this.generarHorariosPorDia(dia));
+    }
   }
 
-  formatHour(hour: number): string {
-    const hours = Math.floor(hour);
-    const minutes = (hour % 1) * 60;
-    return minutes > 0 ? `${hours}:${minutes === 30 ? '30' : '00'}` : `${hours}:00`;
+  generarHorariosPorDia(dia: string) {
+    const horasPorDia = [];
+    let horaActual = 9;
+    const horaFinDia = 23;
+    const duracion = this.duracionesPorDia[dia];
+
+    while (horaActual + duracion <= horaFinDia) {
+      horasPorDia.push(horaActual);
+      horaActual += duracion;
+    }
+
+    this.horariosPorDia[dia] = horasPorDia;
+    this.horariosSeleccionados[dia] = this.horariosSeleccionados[dia] || [];
   }
+
+  toggleHorario(dia: string, hora: number) {
+    const duracion = this.duracionesPorDia[dia];
+    const horaFin = hora + duracion;
+
+    // Validar que la hora de fin no exceda las 23:00
+    if (horaFin > 23) {
+      this.snackBar.open('El horario no puede superar las 23:00', 'Cerrar', { duration: 2000 });
+      return; // No permitir la selección si excede las 23:00
+    }
+
+    const index = this.horariosSeleccionados[dia].findIndex(h => h.inicio === hora);
+
+    if (index === -1) {
+      this.horariosSeleccionados[dia].push({ inicio: hora, fin: horaFin });
+    } else {
+      this.horariosSeleccionados[dia].splice(index, 1);
+    }
+  }
+
+
+  guardarHorariosPorDia() {
+    const horariosDiaSeleccionado = this.horariosSeleccionados[this.diaSeleccionado].map(h => ({
+      dia: this.diaSeleccionado,
+      inicio: h.inicio,
+      fin: h.fin
+    }));
+
+    this.newSector.horarios = this.newSector.horarios.filter(h => h.dia !== this.diaSeleccionado);
+    this.newSector.horarios.push(...horariosDiaSeleccionado);
+
+    this.snackBar.open(`Horarios para ${this.diaSeleccionado} guardados`, 'Cerrar', { duration: 2000 });
+    this.cerrarModal();
+  }
+
+
+
 }
